b76bd430fbdda71db5146370e9f65a3ad3a27261
Pratham Agrawal <58286330+prathamagrawal@users.noreply.github.com>
2022-04-21 15:18:56 +0530

    Added 2 Maps to visualise dataset

--- app.py
+++ app.py
@@ -1,28 +1,63 @@
 from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.express as px
+import json
 
-avocado = pd.read_csv('data.csv')
+
+data = pd.read_csv('data.csv')
 
 app = Dash()
-geo_dropdown = dcc.Dropdown(options=avocado['Year'].unique(),
+geo_dropdown = dcc.Dropdown(options=data['Year'].unique(),
                             value='New York')
-
-app.layout = html.Div(children=[html.H1(
-    children='Dashboard'), geo_dropdown, dcc.Graph(id='price-graph')])
 
 
 @app.callback(
-    Output(component_id='price-graph', component_property='figure'),
+    Output(component_id='graph-02', component_property='figure'),
     Input(component_id=geo_dropdown, component_property='value')
 )
 def update_graph(selected_geography):
-    filtered_avocado = avocado[avocado['Year'] == selected_geography]
-    line_fig = px.line(filtered_avocado,
+    filtered_data = data[data['Year'] == selected_geography]
+    line_fig = px.bar(filtered_data,
                        x='Age', y='Medal',
                        color='Team',
-                       title=f'Avocado Prices in {selected_geography}')
+                       title=f'No. of Medals won by a Country in {selected_geography}')
     return line_fig
+
+
+
+fig = px.choropleth(locationmode="USA-states", color=[1], scope="usa")
+ 
+
+#app.layout = html.Div(childern=[dcc.Graph(id="life-exp-vs-gdp", figure=fig),[html.H1(children='Dashboard'), geo_dropdown, dcc.Graph(id='price-graph')])])
+
+app.layout = html.Div(children=[
+    # All elements from the top of the page
+    html.Div([
+        html.H1(children='Dashboard- Graph 01'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='graph1',
+            figure=fig
+        ),  
+    ]),
+    # New Div for all elements in the new 'row' of the page
+    html.Div([
+        html.H1(children='Graph 02'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+        geo_dropdown,
+        dcc.Graph(
+            id='graph-02',
+            figure=fig
+        ),  
+    ]),
+])
 
 
 if __name__ == '__main__':
