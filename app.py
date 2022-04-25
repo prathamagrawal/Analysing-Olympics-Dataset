93299c54cad708aeb6dcbda0d3476bd6158dacf1
Pratham Agrawal <58286330+prathamagrawal@users.noreply.github.com>
2022-04-25 21:30:22 +0530

    Themed the application with colors and background

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ from wordcloud import WordCloud
 data = pd.read_csv('data.csv')
 colorscales = px.colors.named_colorscales()
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+
 app = Dash()
 values_year = data['Year'].unique()
 values_year.sort()
@@ -35,12 +41,11 @@ wordcloud = WordCloud(
     normalize_plurals=False,
     repeat=False,
     random_state=42,
-    background_color='white')
+    background_color='black')
 wordcloud.generate_from_frequencies(frequencies=count_discipline)
 plt.figure(figsize=(17, 10))
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
-plt.title('Number of athletes by sport', fontsize=30)
 plt.savefig("wordcloud.png")
 
 test_png = 'wordcloud.png'
@@ -49,38 +54,65 @@ test_base64 = base64.b64encode(open(test_png, 'rb').read()).decode('ascii')
 # Graph 2020
 top_countries = data.Team.value_counts().sort_values(ascending=False).head(1203)
 u_team = data.Team.unique()
-fig_graph_2 = px.treemap(path=[u_team], values=top_countries)
+fig_graph_2 = px.treemap(path=[u_team], values=top_countries,color_discrete_sequence=px.colors.sequential.RdBu,template="plotly_dark")
 
-app.layout = html.Div(children=[
+#Graph Age_Medal
+data_age=list(data.Age)
+data_medal=list(data.Medal)
+for i in range(len(data_medal)):
+    if(data_medal[i]>0):
+        data_medal[i]=1
+
+data_pie=pd.DataFrame({"Age":data_age, "Medal":data_medal})
+result=data_pie.sort_values('Age')
+result.drop(result[result['Age'] > 70].index, inplace = True)
+fig_age_medal = px.pie(result, values='Medal', names='Age',
+    title="Age-Medal Distribution",color_discrete_sequence=px.colors.sequential.RdBu,
+    hover_data=['Age'],template = "plotly_dark",)
+fig_age_medal.update_traces(textposition='inside', textinfo='percent+label')
+fig_age_medal.update_layout(margin=dict(t=30, b=30, l=30, r=30))
+
+
+app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
     html.Div([
-        html.H1("Welcome to Analysis of Olympics Dataset",style={"font-family":"montserrat"})
+        html.H1("Welcome to Analysis of Olympics Dataset",style={"font-family":"montserrat","color":"white"})
     ]),
     html.Div([
-        html.H4('Enter a year you want to see the Number of Participation:',style={"font-family":"montserrat"}),
+        html.H4('Enter a year you want to see the Number of Participation:',style={"font-family":"montserrat","color":"white"}),
         dcc.Dropdown(
             id="dropdown",
             options=values_year,
-            value="2020",
             clearable=False,
+            style = {"background-color":"black","text":"white"}
         ),
-        dcc.Graph(id="graph1"),
+        dcc.Graph(id="graph1",
+            figure={
+                'layout':{
+                    'plot_bgcolor': colors['background'],
+                    'paper_bgcolor': colors['background'],
+                    'font': {
+                        'color': colors['text']
+                    }
+                }
+            }
+        )
     ]),
     html.Div([
-        html.H4('The Number of the participation Country wise over the years: ->',style={"font-family":"montserrat"}),
+        html.H4('The Age - Medal Distribution',style={"font-family":"montserrat","color":"white"}),
+        dcc.Graph(figure=fig_age_medal),
+    ]),
+    html.Div([
+        html.H4('The Number of the participation Country wise over the years: ->',style={"font-family":"montserrat","color":"white"}),
         dcc.Graph(figure=fig_graph_2),
     ]),
-
-
     html.Div([
-        html.H1("WordCloud representing the Sports",style={"font-family":"montserrat"}),
+        html.H1("WordCloud representing the Sports",style={"font-family":"montserrat","color":"white"}),
         html.Img(src='data:image/png;base64,{}'.format(test_base64),
                  style={'height': '80%', 'width': '80%', "float": "center"}),
     ])
 ])
 
 # Graph 1
-
-
 @app.callback(
     Output("graph1", "figure"),
     Input("dropdown", "value"))
@@ -91,7 +123,7 @@ def update_bar_chart(year):
     values = top_countries.values
     graph1data = pd.DataFrame({"Teams": indexes, "Participation": values})
     fig = px.bar(graph1data, x="Teams", y="Participation",
-                 title='Participation', template='plotly_dark', color="Teams")
+                 title='Participation', template='plotly_dark', color="Teams",color_discrete_sequence=px.colors.sequential.RdBu)
     return fig
 
 
